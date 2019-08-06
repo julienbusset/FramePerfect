dd8eedef1421c5d26a16a0ae3b9b8db176e5cc55
Julien Busset <julien.busset@gmail.com>
2019-08-06 22:32:54 +0200

    timing data directly in frameperfect.js, and logging it

--- src/frameperfect.js
+++ src/frameperfect.js
@@ -1,3 +1,42 @@
 $(document).ready(function () {
-
+    console.log(Timing());
 });
+
+
+// fonction pour donner le timing d'un mp3 scripté dans un JSON
+function Timing () {
+    return {
+        "title": "Test",
+        "description": "timing des nombres dits dans le mp3",
+        "times": [
+          {
+            "name": "un",
+            "timing": 868
+          },
+          {
+            "name": "un",
+            "timing": 1666
+          },
+          {
+            "name": "deux",
+            "timing": 1903
+          },
+          {
+            "name": "deux",
+            "timing": 2856
+          },
+          {
+            "name": "trois",
+            "timing": 3568
+          },
+          {
+            "name": "un",
+            "timing": 4561
+          },
+          {
+            "name": "trois",
+            "timing": 5345
+          }
+        ]
+        };
+}
