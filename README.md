8316c895475f079442d0dc2e35814b72b7cac6ad
Julien Busset <julien.busset@gmail.com>
2019-08-07 16:15:56 +0200

    Synchro OK entre son et affichage de textes

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # FramePerfect
-javascript library for audio / image sync
+##javascript library for audio / image sync
 
+Not operable right now, but in development. Feel free to contact me for contributing or helping.
 
-Create a "dependencies" folder at root of project, and import dependencies in it :
+By now, if you want to have a look, you can:
+1. Create a "dependencies" folder at root of project, and import dependencies in it :
 - jQuery : https://code.jquery.com/jquery-3.4.1.js
+2. Open Demo/demo.html in your browser
--- src/frameperfect.js
+++ src/frameperfect.js
@@ -1,42 +1,73 @@
 $(document).ready(function () {
-    console.log(Timing());
+  var timing = Timing();
+
+  var startButton = $("#startButton");
+  startButton.click(function () {
+    startSound(timing);
+  });
 });
 
+// démarre le son et les actions synchros
+function startSound(timing) {
+  var audioPlayer = $("#audioPlayer");
+  if (audioPlayer.ready) {
+    audioPlayer.trigger("play");
 
-// fonction pour donner le timing d'un mp3 scripté dans un JSON
-function Timing () {
-    return {
-        "title": "Test",
-        "description": "timing des nombres dits dans le mp3",
-        "times": [
-          {
-            "name": "un",
-            "timing": 868
-          },
-          {
-            "name": "un",
-            "timing": 1666
-          },
-          {
-            "name": "deux",
-            "timing": 1903
-          },
-          {
-            "name": "deux",
-            "timing": 2856
-          },
-          {
-            "name": "trois",
-            "timing": 3568
-          },
-          {
-            "name": "un",
-            "timing": 4561
-          },
-          {
-            "name": "trois",
-            "timing": 5345
-          }
-        ]
-        };
+    for (var i = 0; i < timing.times.length; i++) {
+      showAfterTiming(timing.times[i].name, timing.times[i].timing);
+    }
+  }
+}
+
+// Promise to wait the right timing
+function resolveAfterXms(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, x);
+  });
+}
+
+// async function which render the times[i].name after it's times[i].timing.
+async function showAfterTiming(whatToShow, timingToShow) {
+  await resolveAfterXms(timingToShow);
+  $("#p1").append(whatToShow + " ");
+}
+
+// function to give the timing of a scripted mp3 (JSON)
+function Timing() {
+  return {
+    "title": "Test",
+    "description": "timing des nombres dits dans le mp3",
+    "times": [
+      {
+        "name": "un",
+        "timing": 868
+      },
+      {
+        "name": "un",
+        "timing": 1666
+      },
+      {
+        "name": "deux",
+        "timing": 1903
+      },
+      {
+        "name": "deux",
+        "timing": 2856
+      },
+      {
+        "name": "trois",
+        "timing": 3568
+      },
+      {
+        "name": "un",
+        "timing": 4561
+      },
+      {
+        "name": "trois",
+        "timing": 5345
+      }
+    ]
+  };
 }
